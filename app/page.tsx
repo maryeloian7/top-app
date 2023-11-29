9afc32b3086cbfa5c88e8f8971eac697a4d7355c
Maryia <maritsydik@gmail.com>
2023-11-29 14:45:30 +0100

    continuing work on buttons

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h1">Text</Htag>
-      <Button appearance="primaty">button</Button>
+      <Button appearance="primaty" arrow="right">button</Button>
+      <Button appearance="ghost" >Кнопка</Button>
     </div>
   );
 }
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,15 +2,32 @@ import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
 import cn from "classnames";
 
-export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
+export const Button = ({
+  appearance,
+  arrow = "none",
+  children,
+  className,
+  ...props
+}: ButtonProps): JSX.Element => {
   return (
     <button
-      className={cn(styles.button, {
+      className={cn(styles.button, className, {
         [styles.primary]: appearance == "primaty",
         [styles.ghost]: appearance == "ghost",
       })}
+      {...props}
     >
       {children}
+      {arrow !== "none" && (
+        <span
+          className={cn(styles.arrow, {
+            [styles.down]: arrow == "down",
+            //  [styles.down] : arrow == 'down',
+          })}
+        >
+          <img src="/Arrow.svg" alt="arrow"/>
+        </span>
+      )}
     </button>
   );
 };
